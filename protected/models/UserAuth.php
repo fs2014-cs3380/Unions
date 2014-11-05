7df961665dce42e0f6b68cff9395875e71f52c61
Allen Ahner <ahnera@missouri.edu>
2014-11-05 15:28:42 -0600

    Added the UserAuth model.

---
+++ protected/models/UserAuth.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This is the model class for table "user_auth".
+ *
+ * The followings are the available columns in table 'user_auth':
+ * @property integer $user_id
+ * @property string $password_hash
+ * @property string $salt
+ *
+ * The followings are the available model relations:
+ * @property User $user
+ */
+class UserAuth extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'user_auth';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('user_id, password_hash, salt', 'required'),
+			array('user_id', 'numerical', 'integerOnly'=>true),
+			array('password_hash, salt', 'length', 'max'=>40),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('user_id, password_hash, salt', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'user' => array(self::BELONGS_TO, 'User', 'user_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'user_id' => 'User',
+			'password_hash' => 'Password Hash',
+			'salt' => 'Salt',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('user_id',$this->user_id);
+		$criteria->compare('password_hash',$this->password_hash,true);
+		$criteria->compare('salt',$this->salt,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return UserAuth the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
