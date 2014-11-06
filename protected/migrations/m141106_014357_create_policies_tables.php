694b3e8d11890c59f6c7666b89e654350d0bb7d9
Allen Ahner <ahnera@missouri.edu>
2014-11-05 19:45:15 -0600

    Reconfigured yiic.php file path for the yiic command and added migration for the policy tables.

---
+++ protected/migrations/m141106_014357_create_policies_tables.php
@@ -0,0 +1,25 @@
+<?php
+
+class m141106_014357_create_policies_tables extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m141106_014357_create_policies_tables does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
