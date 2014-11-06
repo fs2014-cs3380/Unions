b6a214702a82ac55ca0ab43f09a28b862d316478
Allen Ahner <ahnera@missouri.edu>
2014-11-05 19:15:20 -0600

    Added db configuration for the console.

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,26 +4,21 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
+	'name'=>'Unions Console',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
 
 	// application components
 	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
+        'db'=>array(
+            'connectionString' => 'mysql:host=127.0.0.1;dbname=unions',
+            'emulatePrepare' => true,
+            'username' => 'unions',
+            'password' => 'iVIOwk*ivx',
+            'charset' => 'utf8',
+        ),
+
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
