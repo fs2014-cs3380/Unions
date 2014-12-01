a9e300276208e2eac0c05743a7e295a40700dd69
Allen Ahner <allenahner@gmail.com>
2014-12-01 11:22:00 -0600

    Removed login and home links (for presentation)

--- babbage/includes/menu_bar.php
+++ babbage/includes/menu_bar.php
@@ -1,8 +1,8 @@
 				<nav>
 					<ul class="topnav">
-						<li><a href="<?php echo MAINDIR ?>">Home</a></li>
+                        <!--<li><a href="<?php /*echo MAINDIR */ ?>">Home</a></li>-->
 						<li><a href="<?php echo MAINDIR ?>">Buildings</a></li>
-						<li><a href="<?php echo MAINDIR ?>">Login/Logout</a></li>
+						<!--<li><a href="<?php /*echo MAINDIR */?>">Login/Logout</a></li>-->
 						<li><a href="<?php echo MAINDIR ?>reservearoom/reservearoom.php">Reserve a Space</a></li>
 						<li>
 							<a href="http://argo.col.missouri.edu/lostandfound">Lost and Found</a>
